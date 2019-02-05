d12bfee7dd41b8625b2f8bf10faecb38485fc3cb
Adam Hawley <adamhawley99@gmail.com>
2019-02-05 22:56:26 +0000

    Added variables to represent acceleration and braking

--- core/src/main/java/uk/ac/york/sepr4/object/entity/LivingEntity.java
+++ core/src/main/java/uk/ac/york/sepr4/object/entity/LivingEntity.java
@@ -15,7 +15,7 @@ public abstract class LivingEntity extends Entity {
     private Double health = 20.0, maxHealth = 20.0, damage = 5.0;
     private boolean isAccelerating, isBraking, isDead, isDying;
     private Integer turningSpeed = 2;
-    private float currentCooldown = 0f, reqCooldown = 0.5f, maxSpeed = 100f, angularSpeed = 0f;
+    private float currentCooldown = 0f, reqCooldown = 0.5f, maxSpeed = 100f, angularSpeed = 0f, acceleration = 40f, deceleration = 80f;
 
     //TODO: Better ways to monitor this
     private int collidedWithIsland = 0, colliedWithBoat = 0;
@@ -74,11 +74,11 @@ public abstract class LivingEntity extends Entity {
                 if (speed > maxSpeed) {
                     speed = maxSpeed;
                 } else {
-                    speed += 40f * deltaTime;
+                    speed += acceleration * deltaTime;
                 }
             } else if (isBraking) {
                 if (speed > 0) {
-                    speed -= 80f * deltaTime;
+                    speed -= deceleration * deltaTime;
                 }
             } else {
                 if (speed > 0) {
